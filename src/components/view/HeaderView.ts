7ff9a046cbae83e1e8c4039f2794a31c256d8031
mogilnitskaya <m_mogilnitskaya@mail.ru>
2025-06-06 02:44:43 +0300

    review issues 2

--- src/components/view/HeaderView.ts
+++ src/components/view/HeaderView.ts
@@ -1,22 +1,28 @@
-import { Component } from "../base/components";
-import { IEvents } from "../base/events";
-import { ensureElement } from "../../utils/utils";
+import { Component } from '../base/components';
+import { IEvents } from '../base/events';
+import { ensureElement } from '../../utils/utils';
 
 export class HeaderView extends Component<HTMLElement> {
-    protected basketButton: HTMLButtonElement;
-    protected counter: HTMLElement;
+	protected basketButton: HTMLButtonElement;
+	protected counter: HTMLElement;
 
-    constructor(container: HTMLElement, events: IEvents) {
-        super(container);
-        this.basketButton = ensureElement<HTMLButtonElement>('.header__basket', container);
-        this.counter = ensureElement<HTMLElement>('.header__basket-counter', container);
-        
-        this.basketButton.addEventListener('click', () => {
-            events.emit('basket:open');
-        });
-    }
+	constructor(container: HTMLElement, events: IEvents) {
+		super(container);
+		this.basketButton = ensureElement<HTMLButtonElement>(
+			'.header__basket',
+			container
+		);
+		this.counter = ensureElement<HTMLElement>(
+			'.header__basket-counter',
+			container
+		);
 
-    set counterValue(value: number) {
-        this.counter.textContent = value.toString();
-    }
+		this.basketButton.addEventListener('click', () => {
+			events.emit('basket:open');
+		});
+	}
+
+	set counterValue(value: number) {
+		this.counter.textContent = value.toString();
+	}
 }
